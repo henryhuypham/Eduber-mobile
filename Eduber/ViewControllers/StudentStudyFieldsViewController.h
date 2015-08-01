fc57508c26a0cfe91c25b14cf945301d9dd70a40
Thong Pham <mr.bamboo.vn@gmail.com>
2015-08-01 22:41:11 +0700

    add shadow view

--- Eduber/ViewControllers/StudentStudyFieldsViewController.h
+++ Eduber/ViewControllers/StudentStudyFieldsViewController.h
@@ -10,6 +10,14 @@
 
 @interface StudentStudyFieldsViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UIView *englishView;
+@property (weak, nonatomic) IBOutlet UIView *mathView;
+@property (weak, nonatomic) IBOutlet UIView *physicView;
+@property (weak, nonatomic) IBOutlet UIView *chemistryView;
+@property (weak, nonatomic) IBOutlet UIView *geographicView;
+@property (weak, nonatomic) IBOutlet UIView *historyView;
+
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *scrollviewWidth;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *scrollviewSidePadding;
