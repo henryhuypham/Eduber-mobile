a2400f29e4324fb638b98b1865b7c4382eab84d8
Huy Pham <huyphamthanh8290@gmail.com>
2015-08-02 00:37:21 +0700

    Teacher class list UI

---
+++ Eduber/Models/ClassInfo.h
@@ -0,0 +1,13 @@
+//
+//  ClassInfo.h
+//  Eduber
+//
+//  Created by Huy Pham on 08/02/15.
+//  Copyright (c) 2015 RTH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClassInfo : NSObject
+
+@end
---
+++ Eduber/ViewControllers/TeacherClassListViewController.m
@@ -0,0 +1,23 @@
+//
+//  TeacherClassListViewController.m
+//  Eduber
+//
+//  Created by Huy Pham on 08/01/15.
+//  Copyright (c) 2015 RTH. All rights reserved.
+//
+
+#import "TeacherClassListViewController.h"
+
+@interface TeacherClassListViewController ()
+
+@end
+
+@implementation TeacherClassListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+}
+
+@end
